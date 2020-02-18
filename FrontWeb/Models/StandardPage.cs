9d434f59a06ab3287600c0ddf4330dba2ba95ba3
Håkan Edling <hakan@tidyui.com>
2020-02-18 13:06:51 +0100

    Initial commit

---
+++ FrontWeb/Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace FrontWeb.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
